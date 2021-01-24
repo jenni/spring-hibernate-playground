dfbff5fab38998f26ee4233e23213dd38e50825d
Jen Varela Lijó <jen.lijo@zalando.de>
2021-01-24 10:35:55 +0100

    spring boot cookbook: scheduled executor

--- src/main/java/com/playground/springhibernate/StartupRunner.java
+++ src/main/java/com/playground/springhibernate/StartupRunner.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 
 public class StartupRunner implements CommandLineRunner {
     protected final Log logger = LogFactory.getLog(getClass());
@@ -14,6 +15,11 @@ public class StartupRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        logger.info("Number of books: " + bookRepository.count());
+    }
+
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    public void run() {
         logger.info("Number of books: " + bookRepository.count());
     }
 }
